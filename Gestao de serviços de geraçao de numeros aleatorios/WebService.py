40e04945ce4b70c97003bab9984d5361d959fc25
PauloProença <93132193+PJProenca@users.noreply.github.com>
2023-05-03 16:31:00 +0100

    Python Code for WebService

---
+++ Gestao de serviços de geraçao de numeros aleatorios/WebService.py
@@ -0,0 +1,60 @@
+import random
+from flask import Flask,jsonify
+import requests
+import json
+import sys
+from datetime import datetime
+
+
+app= Flask(__name__)
+@app.route('/')
+def index():
+     return "Welcome to the Number Generator"
+
+
+def getTemp():
+    api_url = "https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/hp-daily-forecast-day0.json"
+    response = requests.get(api_url)
+    data = json.loads(response.text)
+    data_list = len(data["data"])
+
+    rand_list_item = random.randrange(data_list)
+    temp = data["data"][rand_list_item]["tMax"]
+    return temp
+
+def generateRand ():
+    rand_num = random.randrange(sys.maxsize)
+    return rand_num
+
+@app.route('/bin')
+def binaryConversion():
+    temp_bin = bin(getTemp())
+    rand_bin = bin(generateRand())
+    return temp_bin,rand_bin
+
+def xorCalc():
+    temp_bin = binaryConversion()[0]
+    rand_num_bin = binaryConversion()[1]
+    return int(temp_bin[2:]) ^ int(rand_num_bin[2:])
+
+def overflowHandler(num):
+    treated_num=''
+    for ch in str(num):
+        if ch<='1':
+            treated_num += ch
+    return treated_num
+
+@app.route('/generate')
+def generate():
+    num =xorCalc()
+    bin_num = overflowHandler(num)
+    date= datetime.now().strftime("%d-%B-%Y")
+    time = datetime.now().strftime("%H:%M:%S")
+    data ={'Number': int(bin_num,2),'Binary value':bin_num,'Date':date,'Time':time}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
+
+
+
